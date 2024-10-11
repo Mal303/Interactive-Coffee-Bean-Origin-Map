9adf56cb1b939364e24b3ad5c95e920c59694ed5
Ordannia <rebekkahaa@gmail.com>
2024-10-10 23:23:17 -0400

    Cleaning and Setup

---
+++ SQL/Check_Tables.sql
@@ -0,0 +1,26 @@
+SELECT *
+FROM Arabica_Production;
+
+SELECT *
+FROM Coffee_Production;
+
+SELECT *
+FROM Green_Coffee_Bean_Exports;
+
+SELECT *
+FROM Green_Coffee_Bean_Imports;
+
+SELECT *
+FROM Robusta_Coffee_Production;
+
+SELECT *
+FROM Soluble_Coffee_Exports;
+
+SELECT *
+FROM Soluble_Coffee_Imports;
+
+SELECT *
+FROM Total_Coffee_Exports;
+
+SELECT *
+FROM Total_Coffee_Imports;
